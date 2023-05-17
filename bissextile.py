30563c94d8a59cc06afec404e09d58655fe06797
Adeleke_hanan <junioradeleke03@gmail.com>
2023-05-17 12:45:58 +0100

    Modification de PRINT

--- bissextile.py
+++ bissextile.py
@@ -1,7 +1,7 @@
 annee = input("Veuillez entrer une année dont vous voulez savoir si c'est bissextile ou pas :")
 annee = int(annee)
 if (annee%4) != 0 :
-    print("Cette année n'est pas bissextile")
+    print("Cette année n'est pas une année bissextile")
 elif (annee%100) == 0 :
     if (annee%400) == 0 :
         print("Cette année est bissextile")
