f9343e8fc8e908251a7200ad61398eec274a44a3
Mosaid <msddaouda@gmail.com>
2023-05-17 12:33:02 +0100

    Projet bissextile V1

---
+++ bissextile.py
@@ -0,0 +1,11 @@
+annee = input("Veuillez entrer une année dont vous voulez savoir si c'est bissextile ou pas :")
+annee = int(annee)
+if (annee%4) != 0 :
+    print("Cette année n'est pas bissextile")
+elif (annee%100) == 0 :
+    if (annee%400) == 0 :
+        print("Cette année est bissextile")
+    else :
+        print("Cette année n'est pas bissextile")
+else : 
+    print("Cette année est bissextile")        
